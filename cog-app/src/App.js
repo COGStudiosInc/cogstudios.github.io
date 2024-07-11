b3bf3cc0ebde6a8362f4c527bba2e6d4f2c15ea0
Eluviian <77465461+Eluviian@users.noreply.github.com>
2024-07-11 17:30:45 -0600

    started on the header

--- cog-app/src/App.js
+++ cog-app/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="title">COG Studios Inc</div>
+        <div className="menu"><Menu></Menu></div>
       </header>
+      <div className="bodysection"></div>
     </div>
   );
 }
---
+++ cog-app/src/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+const Menu = () => {
+    return (
+        <div className="menu">
+
+            <span className="menuitem">Home</span>
+            <span className="menuitem">About</span>
+            <span className="menuitem">Playtest</span>
+            <span className="menuitem">Contact</span>
+        
+        </div>
+    );
+  }
+  
+  export default Menu;
