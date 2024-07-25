8f8fbb4d1145a80c64d426086c4440b27417eb2a
Eluviian <77465461+Eluviian@users.noreply.github.com>
2024-07-25 14:52:29 -0600

    started about section

--- cog-app/src/About.js
+++ cog-app/src/About.js
@@ -1,6 +1,29 @@
 import React from "react";
+import './App.css';
 const About = () => {
-    return <h1>about</h1>;
+    const stringPrefix = "/cogstudios.github.io"
+    return <div className="aboutsection">
+        <div className="sectionheader"><h1 className="sectiontitle">Our Story</h1>   <img src={stringPrefix+"/enemy_icon.png"} alt="enemy image" className="aboutimage"></img>  <img src="enemy_icon.png" alt="enemy image" className="aboutimage"></img></div>
+        
+        <div className="sectioncontent">
+            <div>
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Molestie litora pretium amet senectus non. Faucibus fames maecenas leo pulvinar a hendrerit. Penatibus habitant dignissim bibendum molestie, dolor mauris interdum cras. Integer imperdiet pharetra posuere placerat posuere, leo fringilla massa. Orci efficitur volutpat montes consectetur consequat at. Hac iaculis pellentesque netus commodo tempor curae. Nibh cubilia interdum curae semper ipsum dapibus.
+            <br></br><br></br>
+            Pulvinar lobortis turpis non feugiat sapien molestie mus parturient. Id sollicitudin est pellentesque viverra dapibus magna. Molestie lectus ipsum mauris ipsum; quisque tincidunt integer. Quam magnis a adipiscing finibus ac venenatis rhoncus magna ac. Conubia pellentesque litora dictum faucibus cubilia vel. Potenti taciti mus enim; semper mollis tempus mattis quis. Proin felis rhoncus cursus laoreet nam class. Convallis lectus id tellus vulputate facilisi, vulputate class sociosqu. Sollicitudin vestibulum purus vivamus convallis mi ad habitant commodo.
+            <br></br><br></br>
+            Vel lacinia auctor interdum venenatis odio; nunc massa tortor. Ridiculus elementum volutpat, dictum suscipit maximus morbi tempor. Efficitur libero ligula interdum a nascetur ac at lorem. Curae arcu quis per mollis dignissim; nisi a. Congue vel curabitur convallis nec enim blandit. Nascetur mauris proin eros diam vel aptent purus bibendum natoque. Nulla maecenas dui fusce mi inceptos pretium ut facilisis. Ex taciti leo ac vel cursus, pharetra tellus nibh.
+            </div>
+
+            
+
+
+        </div>
+
+
+
+
+
+    </div>
 }
 
 
